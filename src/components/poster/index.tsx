3e43118386eef2e38059854da7b3cd123c90f3e4
tumiMsaki <245551699@qq.com>
2019-11-08 10:11:42 +0800

    selectSort

--- src/components/poster/index.tsx
+++ src/components/poster/index.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react'
+import { useStore } from 'react-redux'
 import './index.scss'
 import store from '../../store'
 
 const Carousel: React.FC = () => {
   store.subscribe(()=> {
-    console.log(store.getState())
+    console.log(count.getState().input.text)
   })
+  const count = useStore()
   return <>
     <div className="poster_app">
       <div className="poster_pic"/>
+      <div>{count.getState().input.text}</div>
     </div>
   </>
 }
