3065b31bafab6b52734d08e852fe609737bf9691
tumiMsaki <245551699@qq.com>
2020-01-15 22:47:33 +0800

    recommend

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,33 +1,39 @@
-import React, { useEffect } from 'react';
-import Slider from '../../components/slider/';
-import { connect } from "react-redux";
-import * as actionTypes from './store/actionCreators';
-import RecommendList from '../../components/list/';
-import Scroll from '../../baseUI/scroll/index';
-import { Content } from './style';
+import React, { useEffect } from 'react'
+import Slider from '../../components/slider/'
+import Loading from '../../baseUI/loading/index';
+import { connect } from "react-redux"
+import { forceCheck } from 'react-lazyload'
+import * as actionTypes from './store/actionCreators'
+import RecommendList from '../../components/list/'
+import Scroll from '../../baseUI/scroll/index'
+import { Content } from './style'
 
 function Recommend (props){
-  const { bannerList, recommendList } = props;
+  const { bannerList, recommendList, enterLoading } = props
 
-  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+  const { getBannerDataDispatch, getRecommendListDataDispatch } = props
 
   useEffect (() => {
-    getBannerDataDispatch ();
-    getRecommendListDataDispatch ();
-    //eslint-disable-next-line
+    if (!bannerList.size) {
+      getBannerDataDispatch () 
+    }
+    if (!recommendList.size) {
+      getRecommendListDataDispatch ()
+    }
   }, []);
 
-  const bannerListJS = bannerList ? bannerList.toJS () : [];
-  const recommendListJS = recommendList ? recommendList.toJS () :[];
+  const bannerListJS = bannerList ? bannerList.toJS () : []
+  const recommendListJS = recommendList ? recommendList.toJS () :[]
 
   return (
     <Content>
-      <Scroll>
+      <Scroll className="list" onScroll={ forceCheck } >
         <div>
           <Slider bannerList={bannerListJS}></Slider>
           <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
+      {enterLoading ? <Loading></Loading> : null}
     </Content> 
   );
 }
@@ -38,18 +44,19 @@ const mapStateToProps = (state) => ({
   // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
   bannerList: state.getIn (['recommend', 'bannerList']),
   recommendList: state.getIn (['recommend', 'recommendList']),
-});
+  enterLoading: state.getIn(['recommend', 'enterLoading'])
+})
 // 映射 dispatch 到 props 上
 const mapDispatchToProps = (dispatch) => {
   return {
     getBannerDataDispatch () {
-      dispatch (actionTypes.getBannerList ());
+      dispatch (actionTypes.getBannerList ())
     },
     getRecommendListDataDispatch () {
-      dispatch (actionTypes.getRecommendList ());
+      dispatch (actionTypes.getRecommendList ())
     },
   }
 };
 
 // 将 ui 组件包装成容器组件
-export default connect (mapStateToProps, mapDispatchToProps)(React.memo (Recommend));
+export default connect (mapStateToProps, mapDispatchToProps)(React.memo (Recommend))
